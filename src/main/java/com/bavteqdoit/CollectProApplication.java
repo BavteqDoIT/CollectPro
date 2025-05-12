e863372b3408b7e4541b8c9ff57583505582cf3c
BavteqDoIT <bartek.adamek@onet.pl>
2025-05-12 16:34:30 +0200

    Initial commit

---
+++ src/main/java/com/bavteqdoit/CollectProApplication.java
@@ -0,0 +1,13 @@
+package com.bavteqdoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bavteqdoit/CollectProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bavteqdoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
